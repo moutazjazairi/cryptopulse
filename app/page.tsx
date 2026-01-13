70f8ad63e68590e0920aacb59e1fcdae6e11b4ef
moutaz jazairi <moutazjazairi77@gmail.com>
2026-01-13 14:58:36 +0400

    Implement Navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
+import Header from "@/components/Header"
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    
       <p className="text-3xl text-indigo-500">
-          Hello Cyka
+          
       </p>
-    </div>
+    
   )
 }
 
